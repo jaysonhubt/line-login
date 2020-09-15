26c6e2ca12d0d1e426788446880759dcede5f7f7
Tran Hong Son <son.tranhong@vietis.com.vn>
2020-09-15 17:02:34 +0700

    Fix url

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -10,7 +10,7 @@ class LineController extends Controller
     const AUTHOR_REQUEST_URL = 'https://access.line.me/oauth2/v2.1/authorize';
     const RESPONSE_TYPE = 'code';
     const CLIENT_ID = 1654923778;
-    const REDIRECT_URL = 'http://line.local/auth';
+    const REDIRECT_URL = '/auth';
     const STATE = 'random_string';
     const SCOPE = 'profile%20openid%20email';
     const CLIENT_SECRET = '04f226eb9eae8a57cdcb9fe361c52047';
@@ -19,21 +19,18 @@ class LineController extends Controller
         $url = self::AUTHOR_REQUEST_URL .
             '?response_type=' . self::RESPONSE_TYPE .
             '&client_id=' . self::CLIENT_ID .
-            '&redirect_uri=' . self::REDIRECT_URL .
+            '&redirect_uri=' . config('APP_URL') . self::REDIRECT_URL .
             '&state=' . self::STATE .
             '&scope=' . self::SCOPE;
-
-//        dd($url);
 
         return redirect($url);
     }
 
     public function getAccessToken(Request $request) {
-        dd($request->all());
         HTTP::post('https://api.line.me/oauth2/v2.1/token', [
             'grant_type' => 'authorization_code',
             'code' => $request->code,
-            'redirect_uri' => self::REDIRECT_URL,
+            'redirect_uri' => config('APP_URL') . self::REDIRECT_URL,
             'client_id' => self::CLIENT_ID,
             'client_secret' => self::CLIENT_SECRET
         ]);
