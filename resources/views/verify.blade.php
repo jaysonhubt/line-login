5e56869f42bb8fed009e75ce1b3bd0558d49b14f
Jayson <sontran.hubt@gmail.com>
2020-09-15 23:09:33 +0700

    Verify

--- resources/views/verify.blade.php
+++ resources/views/verify.blade.php
@@ -9,5 +9,15 @@
     <input type="submit" value="Submit">
 </form>
 
+
+<form action="https://api.line.me/oauth2/v2.1/token" method="POST" enctype="application/x-www-form-urlencoded">
+    <input type="hidden" name="grant_type" value="authorization_code">
+    <input type="hidden" name="code" value="{{ $code }}">
+    <input type="hidden" name="redirect_uri" value="{{ route('line_verify') }}">
+    <input type="hidden" name="client_id" value="1654923778">
+    <input type="hidden" name="client_secret" value="04f226eb9eae8a57cdcb9fe361c52047">
+    <input type="submit" value="Submit">
+</form>
+
 </body>
 </html>
