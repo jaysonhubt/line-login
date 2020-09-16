0d568c1af45ee08869eaf5cf09dd8682d127bf72
Tran Hong Son <son.tranhong@vietis.com.vn>
2020-09-16 17:31:08 +0700

    Fix Line messaging api

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -86,7 +86,7 @@ class LineController extends Controller
             'notificationDisabled' => false
         ];
 
-        Log::channel('single')->info($replyContent);
+        Log::channel('9HtT9mHDETFdiWrXX8xTmjauaOMiHDI4IaavUBX59ftcLtkuo64C3TI1g43OxY8Ksq+yBDl5ZeNIfOxlnmSFy6VYubNLTvKxMjQxwVTV1zZiRQUyrmpUmJTjUsXwrjqx02YjHTrZh/AqAE0xK5U6LgdB04t89/1O/w1cDnyilFU=')->info($replyContent);
 
         return HTTP::withToken('token')
             ->post('https://api.line.me/v2/bot/message/reply', $replyContent);
