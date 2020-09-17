be8f4955a1c09a86a23a860f987337e69195f9a1
Tran Hong Son <son.tranhong@vietis.com.vn>
2020-09-17 17:39:08 +0700

    Fix bug

--- app/Helpers/LineHelper.php
+++ app/Helpers/LineHelper.php
@@ -7,7 +7,7 @@ use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineHelper extends LINEBot
 {
-    public function __construct(array $args)
+    public function __construct(array $args = [])
     {
         $httpClient = new CurlHTTPClient(env('CHANNEL_TOKEN'));
         parent::__construct($httpClient, [...$args, ['channelSecret' => env('CHANNEL_SECRET')]]);
