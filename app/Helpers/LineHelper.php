4b833894e16635a6de5057dd9aedae7a726f8b0f
Tran Hong Son <son.tranhong@vietis.com.vn>
2020-09-18 08:45:58 +0700

    Remove message builder

--- app/Helpers/LineHelper.php
+++ app/Helpers/LineHelper.php
@@ -4,15 +4,11 @@ namespace App\Helpers;
 
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use LINE\LINEBot\MessageBuilder;
 
 class LineHelper extends LINEBot
 {
-    public MessageBuilder $messageBuilder;
-
-    public function __construct(MessageBuilder $messageBuilder, array $args = [])
+    public function __construct(array $args = [])
     {
-        $this->messageBuilder = $messageBuilder;
         $httpClient = new CurlHTTPClient(env('CHANNEL_TOKEN'));
         parent::__construct($httpClient, [...$args, 'channelSecret' => env('CHANNEL_SECRET')]);
     }
